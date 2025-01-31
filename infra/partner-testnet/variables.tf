6ac6e14fef94c0d5a51054d969fb0d01aad3fcb9
Peter <petru.papucciu@nearone.org>
2025-01-31 10:33:55 +0000

    Modified terraform recipes and init script

--- infra/partner-testnet/variables.tf
+++ infra/partner-testnet/variables.tf
@@ -17,7 +17,7 @@ variable "mig_name" {
 variable "image" {
   description = "The Docker image to deploy to GCE instances. Note: This is a public image repository used for updating your nodes, please do not change this"
   type        = string
-  default     = "us-east1-docker.pkg.dev/pagoda-discovery-platform-prod/multichain-public/multichain-testnet:latest"
+  default     = "docker.io/nearone/mpc-node-gcp:latest"
 }
 
 variable "image_port" {
@@ -64,18 +64,28 @@ variable "env_variables" {
 
 variable "node_configs" {
   type = list(object({
-    account              = string
-    cipher_pk            = string
-    account_sk_secret_id = string
-    cipher_sk_secret_id  = string
-    sign_sk_secret_id    = string
-    sk_share_secret_id   = string
+    account                            = string
+    cipher_pk                          = string
+    account_sk_secret_id               = string
+    cipher_sk_secret_id                = string
+    sign_sk_secret_id                  = string
+    sk_share_secret_id                 = string
+    gcp_local_encryption_key_secret_id = string
+    gcp_keyshare_secret_id             = string
+    gcp_p2p_private_key_secret_id      = string
+    gcp_account_sk_secret_id           = string
+    mpc_home_dir                       = string
   }))
 }
 
 variable "env" {
   type    = string
   default = "dev"
+}
+
+variable "scenario" {
+  type    = string
+  default = "old"
 }
 
 variable "static_env" {
@@ -90,7 +100,7 @@ variable "static_env" {
     },
     {
       name  = "MPC_CONTRACT_ID"
-      value = "v1.signer-prod.testnet"
+      value = "signer-c436b5b3-f815-4750-938d-1a4b4b87c911.testnet"
     },
     {
       name  = "MPC_INDEXER_S3_BUCKET"
@@ -98,7 +108,7 @@ variable "static_env" {
     },
     {
       name  = "MPC_INDEXER_START_BLOCK_HEIGHT"
-      value = 158767549
+      value = 184330000
     },
     {
       name  = "AWS_DEFAULT_REGION"
@@ -106,7 +116,7 @@ variable "static_env" {
     },
     {
       name  = "MPC_GCP_PROJECT_ID"
-      value = "<your-project-id>"
+      value = "nearone-mpc"
     },
     {
       name  = "MPC_WEB_PORT"
