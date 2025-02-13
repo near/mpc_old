41d33695f47999df1b0ba3b6da329795c6e17223
Peter <petru.papucciu@nearone.org>
2025-02-13 17:40:10 +0000

    Changed default contact it as dev is not maintained

--- chain-signatures/node/src/cli.rs
+++ chain-signatures/node/src/cli.rs
@@ -27,7 +27,7 @@ pub enum Cli {
         )]
         near_rpc: String,
         /// MPC contract id
-        #[arg(long, env("MPC_CONTRACT_ID"), default_value("v1.signer-dev.testnet"))]
+        #[arg(long, env("MPC_CONTRACT_ID"), default_value("v1.signer-prod.testnet"))]
         mpc_contract_id: AccountId,
         /// This node's account id
         #[arg(long, env("MPC_ACCOUNT_ID"))]
